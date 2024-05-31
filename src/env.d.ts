799a46de768b61139f871853e0de5a0070485be0
Amirarsalan Farahanian <amirarsalanfarahanian@gmail.com>
2024-05-31 19:57:09 +0330

    add a standard JSON format for responses

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,29 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NODE_ENV: string;
       SERVER_PORT: number;
+
+      //MONGODB
+      MONGODB_URI: string;
     }
+  }
+
+  interface JSONResponseFormat {
+    success: boolean;
+    message: string;
+    data: [] | {} | null;
+    pagination: {
+      limit: number;
+      offset: number;
+      currentPage: number;
+      totalItems: number;
+      totalPages: number;
+    } | null;
+    error?: {
+      code: number;
+      message: string[];
+    };
   }
 }
 
